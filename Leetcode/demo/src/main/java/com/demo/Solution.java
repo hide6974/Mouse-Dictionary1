3f1d08d62597bc8d08a4688f2484e6858fa04bd9
hide6974 <hide6974@gmail.com>
2021-04-18 08:06:33 +0900

    add learn pro

--- Leetcode/demo/src/main/java/com/demo/Solution.java
+++ Leetcode/demo/src/main/java/com/demo/Solution.java
@@ -12,4 +12,58 @@ class Solution {
         }
         return i + 1;
     }
+
+    public int removeElement(int[] nums,int val) {
+        if (nums.length == 0) return 0;
+        int i = 0;
+        for (int j = 0; j < nums.length; j++) {
+            if (nums[j] != val) {
+                nums[i] = nums[j];
+                i++;
+            }
+        }
+        return i;
+    }
+  
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+    public ListNode removeElements(ListNode head, int val) {
+     
+        //bat output [0,1,2,3,4,5]
+        // ListNode helper = new ListNode(0); 
+        // helper.next = head;
+        // ListNode p = helper;
+        
+        // while(p.next != null){
+        //   if(p.next.val == val){
+        //       ListNode next = p.next;
+        //       p.next = next.next;
+        //   }else{
+        //       p = p.next;
+        //   }
+        //   return helper;
+        // }
+
+        ListNode dummy = new ListNode(0); 
+        dummy.next = head;
+        head = dummy;
+        
+        while(head.next != null){
+          if(head.next.val == val){
+              head.next = head.next.next;
+          }else{
+              head = head.next;
+          }
+        }
+        return dummy.next;
+    }
+    
     }

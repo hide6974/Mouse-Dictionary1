79291b6b4799b9d2058f7469939ca8bcaa2a2c37
hide6974 <hide6974@gmail.com>
2021-04-18 08:06:14 +0900

    'add learn'

--- Leetcode/demo/src/test/java/com/demo/SolutionTest.java
+++ Leetcode/demo/src/test/java/com/demo/SolutionTest.java
@@ -14,14 +14,44 @@ public class SolutionTest {
      * Rigorous Test.
      */
     @Test
-    void testApp() {
+    // void testRemoveDuplicates() {
 
+    //     Solution solution = new Solution();
+    //     int expected = 2;
+    //     int[] param ={1,1,3};
+    //     int actual = 0;
+
+    //     actual = solution.removeDuplicates(param);
+
+    //     assertEquals(expected, actual);
+    // }
+
+    
+    void testRemoveElement() {
         Solution solution = new Solution();
+        int[] param ={3,2,2,3};
+        int val =3;
+    
         int expected = 2;
-        int[] param ={1,1,3};
         int actual = 0;
 
-        actual = solution.removeDuplicates(param)-1;
+        actual = solution.removeElement(param,val);
+
+        assertEquals(expected, actual);
+    }
+
+       
+    void testRemoveElements() {
+        Solution solution = new Solution();
+
+        ListNode head =new ListNode();
+        head. ={1,2,6,3,4,5,6};
+        int val =6;
+    
+        int expected = 2;
+        int actual = 0;
+
+        actual = solution.removeElement(head,val);
 
         assertEquals(expected, actual);
     }

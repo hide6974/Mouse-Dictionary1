991a902dac98662fa2930d095085a7f14a5d9f96
hide6974 <hide6974@gmail.com>
2021-04-21 21:41:37 +0900

    issue

---
+++ GCE/login/app.js
@@ -0,0 +1,59 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+var app = express();
+
+//mongooseをロード
+const mongoose = require("mongoose");
+//データーベース接続を設定
+mongoose.connect(
+  "mongodb://<username>:<password>@localhost:27017/<dbname>",
+  {userNewParser:true}
+);
+
+mongoose.set("useCreateIndex", true);
+
+//データーベースをdb変数に代入
+const db = mongoose.connection;
+
+//接続メッセージをログに出力する
+db.once("open",()=>{
+  console.log("success connect");
+});
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
